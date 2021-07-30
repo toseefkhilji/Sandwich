e66ec0696031ecb7c15e38829f056452a5e030ed
Toseefhusen <toseefhusen@webteamcorp.com>
2021-07-30 16:17:25 +0530

    Initial Commit

---
+++ Sandwich/SandwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichApp.swift
+//  Sandwich
+//
+//  Created by Toseef on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SandwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
